23876212a656eafaaae6ff256fa54606cdfa7ae8
Lari <larileal@hotmail.com.br>
2023-08-01 14:43:13 -0300

    criando um seed para uma tabela já existente

---
+++ Exemplo-DataSeeds/Data/DataSeed/SeedWeatherConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Exemplo_DataSeeds.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Exemplo_DataSeeds.Data.DataSeed
+{
+    public class SeedWeatherConfiguration : IEntityTypeConfiguration<WeatherForecast>
+    {
+        public void Configure(EntityTypeBuilder<WeatherForecast> builder)
+        {
+            builder.HasData(
+                new WeatherForecast { Date = new DateTime(2023, 8, 1), TemperatureC = 25, Summary = "Chuvoso"},
+                new WeatherForecast { Date = new DateTime(2023, 8, 2), TemperatureC = 35, Summary = "Ensolarado" },
+                new WeatherForecast { Date = new DateTime(2023, 8, 3), TemperatureC = 16, Summary = "Nublado" },
+                new WeatherForecast { Date = new DateTime(2023, 8, 4), TemperatureC = 33, Summary = "Ensolarado" }
+            );
+        }
+    }
+}
--- Exemplo-DataSeeds/Data/WeatherContext.cs
+++ Exemplo-DataSeeds/Data/WeatherContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Exemplo_DataSeeds.Data
+﻿using Exemplo_DataSeeds.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exemplo_DataSeeds.Data
 {
-    public class WeatherContext
+    public class WeatherContext : DbContext
     {
+        public WeatherContext(DbContextOptions<WeatherContext> opts) : base(opts)
+        {
+            
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
     }
 }
